81bbd4eefcc0aa6769691ee2db251c29e5c2460f
Patrícia <prrs89@gmail.com>
2018-08-08 09:57:58 +0100

    Add Map and Aside

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    mapIsReady: false,
+  };
+
+  componentDidMount() {
+    const ApiKey = 'AIzaSyCI3eIrMweOmEiMl3DzWe152eYEVbYCqjc';
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${ApiKey}`;
+    script.async = true;
+    script.defer = true;
+    script.addEventListener('load', () => {
+      this.setState({ mapIsReady: true });
+    });
+
+    document.body.appendChild(script);
+  }
+
+  componentDidUpdate() {
+    if (this.state.mapIsReady) {
+      // Display the map
+      this.map = new window.google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 12,
+      });
+      // You also can add markers on the map below
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <aside className="App-aside">
           <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </aside>
+        <div id="map"></div>
       </div>
     );
   }
